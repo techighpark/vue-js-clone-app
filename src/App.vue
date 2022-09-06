49d45de89c370bc814fec7c2debcf3c99696fcaf
Taekwan Park <taekwanpark@Taekwanui-MacBookPro.local>
2022-09-06 14:29:08 +0900

    attribute binding

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+
+  <!-- binding object -->
+  <div>
+    Shopping Mall
+    <h4 :style="style">vue clone coding</h4>
+    <p>{{ price1 }} dollar</p>
+  </div>
+  <!-- biding computed -->
+  <div>
+    <h4 :style="classObject">vue clone coding</h4>
+    <p>{{ price2 }} dollar</p>
+  </div>
+
+  <!-- Binding to Arrays -->
+  <div>
+    <h4 :style="[bgColor, color]">vue clone coding</h4>
+    <p>{{ price2 }} dollar</p>
+  </div>
+  <div>
+    <h4 :style="[bgColor, { color: style.color }]">vue clone coding</h4>
+    <p>{{ price2 }} dollar</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      price1: 60,
+      price2: 70,
+      //binding object
+      style: { backgroundColor: 'black', color: 'red' },
+      //biding computed
+      hasError: true,
+      error: '123',
+      //Binding to Arrays
+      bgColor: 'backgroundColor:green',
+      color: 'color:orange',
+    };
+  },
+  computed: {
+    classObject() {
+      return {
+        backgroundColor: this.hasError ? 'black' : 'red',
+        color: this.error === 'true' ? 'red' : 'yellow',
+      };
+    },
+  },
+  components: {},
+};
 </script>
 
 <style>
